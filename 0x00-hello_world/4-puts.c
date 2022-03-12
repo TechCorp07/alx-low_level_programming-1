64177efe51d21a119c7e6baeb9cdc99096ab7320
Michael-champions <immichaelattah@gmail.com>
2022-03-12 06:30:40 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
+/**
 
-/*
- * main - entry point
- *
- *
- * return: always 0 (success)
+ * main - print the puts message
+ * Return: always 0 (success)
 */
 
-int main(void){
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+
+int main(void)
+
+{
+         puts("\"Programming is like building a multilingual puzzle");
+         return (0);
 }
